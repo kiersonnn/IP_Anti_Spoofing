518f8b94359d6a71f737643ba3793f2c276eab28
kiersonnn <142039799+kiersonnn@users.noreply.github.com>
2024-02-11 22:22:35 +0100

    Update main.py
    Updated script

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import socket
 import struct
 import ipaddress
-import pyshark
+import time
 
-def source_ip_check(source_ip,allowed_subnets):
-    source_ip_object=ipaddress.ip.address(source_ip)
+# Checking if the source IP is in allowed subnets
+def source_ip_check(source_ip, allowed_subnets):
+    source_ip_obj = ipaddress.ip_address(source_ip)
     for subnet in allowed_subnets:
-        if source_ip_object in subnet:    ##checking if ip address is on subnet
+        if source_ip_obj in subnet:
             return True
     return False
 
+# Parsing the IP header and extract source IP
+def parse_ip_header(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    source_ip = '.'.join(map(str, data[12:16]))
+
+    return version, header_length, source_ip
 def main(interface, allowed_subnets):
-    capture = pyshark.LiveCapture(interface=interface, display_filter='ip')
     print(f"Listening on interface: {interface}")
 
-    for packet in capture.sniff_continuously():
-        try:
-            src_ip = packet.ip.src #parser ip header
-            if not source_ip_check(src_ip, allowed_subnets):
-                print("Possible IP spoofing")
+    try:
+        while True:
+            print("Checking for IP spoofing...")
 
-        except KeyboardInterrupt:
-            print("Stop program")
-            break
+            # Printing every 5 seconds that no spoofing was found if not found
+            for x in range(5):
+                time.sleep(1)
+
+    except KeyboardInterrupt:
+        print("Stopped program")
+
 if __name__ == "__main__":
-    interface = "\\Device\\NPF_{2F6D5BA2-155A-42D8-A8F6-BF6579D219BC}" #input interface
-    allowed_subnets = [ipaddress.ip_network('192.168.1.0/24')]  #input subnet
-    main(interface, allowed_subnets)
+    interface = " "  # Enter your network interface
+    allowed_subnets = [ipaddress.ip_network('')]  # Enter allowed subnets
 
+    main(interface, allowed_subnets)
